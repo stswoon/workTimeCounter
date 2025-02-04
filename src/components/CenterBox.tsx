65e893f8fe54cd177ca15aba821d6b1100d72666
stswoon <xx@xx.xx>
2025-02-04 22:09:18 +0400

    fix favicon, add linter, prettier

--- src/components/CenterBox.tsx
+++ src/components/CenterBox.tsx
@@ -1,8 +1,12 @@
-import {FC, memo, PropsWithChildren} from "react";
-import {Box} from "@mui/material";
+import { FC, memo, PropsWithChildren } from "react";
+import { Box } from "@mui/material";
 
-const CenterBox: FC<PropsWithChildren> = ({children}) => (
-    <Box sx={{display: "flex", flexDirection: "column", justifyContent: "center"}}>{children}</Box>
+const CenterBox: FC<PropsWithChildren> = ({ children }) => (
+  <Box
+    sx={{ display: "flex", flexDirection: "column", justifyContent: "center" }}
+  >
+    {children}
+  </Box>
 );
 
 export default memo(CenterBox);
