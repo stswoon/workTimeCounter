dc9b077aab6b67430316f9343796199f99d5aaa6
stswoon <xx@xx.xx>
2025-02-05 23:13:15 +0400

    auto update version

--- build.helper.mjs
+++ build.helper.mjs
@@ -7,14 +7,14 @@ const updatePatchVersion = () => {
   newVersion[2] = parseInt(newVersion[2], 10) + 1;
   newVersion = newVersion.join(".");
   packageJson.version = newVersion;
-  packageJson = JSON.stringify(packageJson, null, 2);
+  packageJson = JSON.stringify(packageJson, null, 2) + "\n";
   fs.writeFileSync("package.json", packageJson, "utf8");
 
   let packageLockJson = fs.readFileSync("package-lock.json", "utf8");
   packageLockJson = JSON.parse(packageLockJson);
   packageLockJson.version = newVersion;
   packageLockJson.packages[""].version = newVersion;
-  packageLockJson = JSON.stringify(packageLockJson, null, 2);
+  packageLockJson = JSON.stringify(packageLockJson, null, 2) + "\n";
   fs.writeFileSync("package-lock.json", packageLockJson, "utf8");
 };
 
